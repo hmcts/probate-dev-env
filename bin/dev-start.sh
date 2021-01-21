fffd9d53fa0f9eccebc4a458b69ea8921316e038
Vincent Holt <vleholt@yahoo.co.uk>
2021-01-21 16:28:59 +0000

    move fee updates to dev start

--- bin/dev-start.sh
+++ bin/dev-start.sh
@@ -24,3 +24,81 @@ echo "Starting dependencies..."
 docker-compose ${COMPOSE_FILE} up -d
 
 echo "LOCAL ENVIRONMENT SUCCESSFULLY STARTED"
+
+echo "Updating fees..."
+
+UPDATE_COUNT=0;
+UPDATE_RESULT="";
+until [[ "$UPDATE_RESULT" == "UPDATE 1" || "$UPDATE_COUNT" == 20 ]]
+do
+  if [ "$UPDATE_RESULT" == "" ]
+  then
+    echo "attempting copies fee keyword update";
+  else 
+    echo "retrying copies fee keyword update";
+  fi
+
+  UPDATE_RESULT=$(psql -h localhost --username postgres -d fees_register -p 5050 -c "UPDATE public.fee SET keyword = 'NewFee' WHERE code = 'FEE0003'";);
+  echo $UPDATE_RESULT
+  if [ "$UPDATE_RESULT" != "UPDATE 1" ]
+  then
+    if [ "$UPDATE_RESULT" == "" ]
+    then 
+      UPDATE_RESULT="ERROR";
+    fi
+    sleep 3;
+  fi
+
+  ((UPDATE_COUNT+=1));
+done
+
+UPDATE_COUNT=0;
+UPDATE_RESULT="";
+until [[ "$UPDATE_RESULT" == "UPDATE 1" || "$UPDATE_COUNT" == 20 ]]
+do
+  if [ "$UPDATE_RESULT" == "" ]
+  then
+    echo "attempting volume_amount amount update";
+  else 
+    echo "retrying volume_amount amount update";
+  fi
+
+  UPDATE_RESULT=$(psql -h localhost --username postgres -d fees_register -p 5050 -c "UPDATE volume_amount SET amount = '1.5' WHERE id = '4'";);
+  echo $UPDATE_RESULT
+  if [ "$UPDATE_RESULT" != "UPDATE 1" ]
+  then
+    if [ "$UPDATE_RESULT" == "" ]
+    then 
+      UPDATE_RESULT="ERROR";
+    fi
+    sleep 3;
+  fi
+
+  ((UPDATE_COUNT+=1));
+done
+
+UPDATE_COUNT=0;
+UPDATE_RESULT="";
+until [[ "$UPDATE_RESULT" == "UPDATE 1" || "$UPDATE_COUNT" == 20 ]]
+do
+  if [ "$UPDATE_RESULT" == "" ]
+  then
+    echo "attempting flat_amount amount update";
+  else 
+    echo "retrying flat_amount amount update";
+  fi
+
+  UPDATE_RESULT=$(psql -h localhost --username postgres -d fees_register -p 5050 -c "UPDATE flat_amount SET amount = '1.5' WHERE id = '1'";);
+  echo $UPDATE_RESULT
+  if [ "$UPDATE_RESULT" != "UPDATE 1" ]
+  then
+    if [ "$UPDATE_RESULT" == "" ]
+    then 
+      UPDATE_RESULT="ERROR";
+    fi
+    sleep 3;
+  fi
+
+  ((UPDATE_COUNT+=1));
+done
+
